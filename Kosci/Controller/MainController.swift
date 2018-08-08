20ff0d80ea7bf413c2851628cda110339325d002
patisonek <schelung@gmail.com>
2018-08-09 00:01:07 +0200

    edit button

--- Kosci/Controller/MainController.swift
+++ Kosci/Controller/MainController.swift
@@ -12,7 +12,7 @@ class MainController: UIViewController {
     var randomDiceIndex1 : Int = 0
     var randomDiceIndex2 : Int = 0
     
-    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
     
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
@@ -36,8 +36,8 @@ class MainController: UIViewController {
         randomDiceIndex1 = Int(arc4random_uniform(6))
         randomDiceIndex2 = Int(arc4random_uniform(6))
         
-        print(randomDiceIndex1)
-        print(randomDiceIndex2)
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
     }
     
 }
