c712bc2fbd136cc02cc05bba96bb569bd948f576
Patisonek <patrycja.warmuz@gmail.com>
2018-08-09 13:58:46 +0200

    add comments

--- Kosci/Controller/MainController.swift
+++ Kosci/Controller/MainController.swift
@@ -12,7 +12,7 @@ class MainController: UIViewController {
     var randomDiceIndex1 : Int = 0
     var randomDiceIndex2 : Int = 0
     
-    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice5", "dice6"]
     
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
@@ -20,25 +20,45 @@ class MainController: UIViewController {
  
     
     
-    
+    // MARK: - View Controller
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        updateDiceImg()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
+    
+    //MARK: - Button
     @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+      updateDiceImg()
+        
+    }
+    
+    //MARK: - My Methods
+    func updateDiceImg(){
         
         randomDiceIndex1 = Int(arc4random_uniform(6))
         randomDiceIndex2 = Int(arc4random_uniform(6))
         
-        print(randomDiceIndex1)
-        print(randomDiceIndex2)
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+        
     }
+    
+  
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        
+        updateDiceImg()
+    }
+    
     
 }
 
